362a4981befe99c0124ad7e680853599bd0888e6
Robby Zhao <robby.zhaox@gmail.com>
2025-05-21 21:58:17 +0800

    update url

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   env: {
-    NEXT_PUBLIC_API_BASE_URL: "http://192.168.0.70:8000"
+    NEXT_PUBLIC_API_BASE_URL: "http://localhost:8000"
   },
 };
 
